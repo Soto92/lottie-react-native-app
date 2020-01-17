950fa215d3ecea8aabc5106f35f6874b6dc8613f
soto92 <mauriciosoto@rede.ulbra.br>
2020-01-17 17:04:31 -0300

    add animations e control

--- App.js
+++ App.js
@@ -5,13 +5,15 @@ import {
   Animated,
   StatusBar,
   Easing,
-  Text,
   SafeAreaView,
-  TouchableOpacity
+  TouchableOpacity,
+  ScrollView
 } from "react-native";
 import LottieView from "lottie-react-native";
 import loading from "./android/app/src/svg/196-material-wave-loading.json";
 import joia from "./android/app/src/svg/thumb-up.json";
+import circle from "./android/app/src/svg/222-trail-loading.json";
+import wind from "./android/app/src/svg/14327-wind-gust.json";
 
 const App: () => React$Node = () => {
   const [progress] = useState(new Animated.Value(0));
@@ -37,39 +39,65 @@ const App: () => React$Node = () => {
   return (
     <SafeAreaView>
       <StatusBar barStyle="dark-content" />
-      <View style={styles.container}>
-        {/* <Text>Hello</Text> */}
-        
-          <LottieView style={{margin: 20}} progress={progress} source={loading} />
-        
-        <View>
-          <TouchableOpacity onPress={() => handleAnime()}>
-            <View
-              style={{
-                backgroundColor: "grey",
-                margin: 10,
-                width: 60,
-                height: 60,
-                borderRadius: 10
-              }}
-            >
-              <LottieView progress={progress} source={joia} />
-            </View>
-          </TouchableOpacity>
+      <ScrollView contentContainerStyle={{ alignItems: "center" }}>
+        <View style={styles.container}>
+          <LottieView
+            style={{ margin: 20 }}
+            autoPlay={true}
+            loop={true}
+            source={circle}
+          />
         </View>
-      </View>
+        <View style={styles.container}>
+          <LottieView
+            style={{ margin: 20 }}
+            autoPlay={true}
+            loop={true}
+            source={wind}
+          />
+        </View>
+        <View style={styles.container}>
+          <LottieView
+            style={{ margin: 20 }}
+            autoPlay={true}
+            loop={true}
+            source={loading}
+          />
+        </View>
+        <View style={styles.container}>
+          <LottieView
+            style={{ margin: 20 }}
+            progress={progress}
+            source={loading}
+          />
+          <View>
+            <TouchableOpacity onPress={() => handleAnime()}>
+              <View
+                style={{
+                  backgroundColor: "grey",
+                  margin: 10,
+                  width: 60,
+                  height: 60,
+                  borderRadius: 10
+                }}
+              >
+                <LottieView progress={progress} source={joia} />
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: "red"
-  },
   container: {
-    alignItems: 'center',
-    backgroundColor: "white",
-    height: 400,
+    alignItems: "center",
+    backgroundColor: "#EFF2F4",
+    borderRadius: 8,
+    margin: 8,
+    height: 300,
     width: 300
   }
 });
